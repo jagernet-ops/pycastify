0d00493d0c1f2e3a209a5b4a9a0ecf5e25fbb1ce
jagernet-ops <mshalewski@tutanota.com>
2023-02-20 22:40:23 -0600

    added fstring syntax as well as generalized code

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ import time
 import pychromecast
 import os
 import subprocess
+import socket
 
-chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=["Mikes Hard Visual Aid"])
+chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[" "]) # Place the name of your chromecast device within the open-close quotes
+
+PORT = 8000
+HOSTNAME = socket.gethostname()
+ADDRESS = socket.gethostbyname(HOSTNAME)
 
 def videoServer():
     videoDirectory = input("What directory is the file stored?: ")
-    command = "python3 -m http.server -d " + videoDirectory + " &"
-    subprocess.run(command, shell=True)
+    command = f"python3 -m http.server -d {videoDirectory} &"
+    server = subprocess.run(command, shell=True)
+    
+    
 
 def videoCast():
     videoTitle = input("What is the filename of the video?: ")
     cast = chromecasts[0]
     cast.wait()
     mc = cast.media_controller
-    videoLink = "http://192.168.1.3:8000/" + videoTitle
+    videoLink = f"http://{ADDRESS}:{PORT}/{videoTitle}"
     mc.play_media(videoLink, 'video/mp4')
     mc.block_until_active()
     print(mc.status)
@@ -27,8 +34,13 @@ while(stopVideo == False):
     userResponse = input("End Server?(y/n): ")
     if(userResponse == "y"):
         stopVideo = True
+        break
     elif(userResponse == "n"):
         print("Resuming server operations?")
     else:
         print("Somethings wrong I can feel it...")
-subprocess.run("pkill python3")
+if(os.name == 'postix'):
+    subprocess.run("pkill python3", shell=True)
+elif(os.name == 'nt'):
+    subprocess.run("taskkill /im python.exe", shell=True)
+print("Exiting")
